e588fc1af6f8215906e056c994ce0989a9686ca6
Micheleen Harris <michhar@microsoft.com>
2021-03-15 09:51:32 -0700

    Wrapped function with name eq main

--- hsv_colorspace_plotter.py
+++ hsv_colorspace_plotter.py
@@ -1,9 +1,10 @@
 """
-HSV Colorspace plot with the following ranges:
+HSV Colorspace plot with the following ranges (these being the OpenCV 
+ranges):
 
 H: 0-179 (hue range)
 S: 0-255 (saturation range)
-V: 255 (constant vibrance)
+V: 255 (value - kept constant)
 
 Use this plot to understand the desired values for the masking of
 a desired color with these projects which use HSV colorspaces.
@@ -13,18 +14,24 @@ Based on: https://stackoverflow.com/questions/10787103/2d-hsv-color-space-in-mat
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import hsv_to_rgb
+import matplotlib
+matplotlib.use('TkAgg')
 
 
-s, h = np.mgrid[0:1:255j, 0:1:179j]
-v = np.ones_like(s)
-hsv = np.dstack((h,s,v))
-rgb = hsv_to_rgb(hsv)
+def draw_hsv_colorspace():
+    s, h = np.mgrid[0:1:255j, 0:1:179j]
+    v = np.ones_like(s)
+    hsv = np.dstack((h,s,v))
+    rgb = hsv_to_rgb(hsv)
 
-print(rgb.shape)
-plt.imshow(rgb, origin="lower", interpolation='nearest', aspect='auto')
-plt.gca().invert_yaxis()
-plt.xlabel("H")
-plt.ylabel("S")
-plt.xticks(range(0,179,20))
-plt.title("$V_{HSV}=255$")
-plt.show()
+    print(rgb.shape)
+    plt.imshow(rgb, origin="lower", interpolation='nearest', aspect='auto')
+    plt.gca().invert_yaxis()
+    plt.xlabel("H")
+    plt.ylabel("S")
+    plt.xticks(range(0,179,20))
+    plt.title("$V_{HSV}=255$")
+    plt.show()
+
+if __name__ == "__main__":
+    draw_hsv_colorspace()
